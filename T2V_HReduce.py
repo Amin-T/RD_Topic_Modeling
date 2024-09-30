d6a98471daa05032634031cd3f2f62faee302480
Amin Tavakkolnia <62079561+Amin-T@users.noreply.github.com>
2024-09-30 15:35:18 +0200

    Update
    Updated python codes

--- T2V_HReduce.py
+++ T2V_HReduce.py
@@ -15,27 +15,32 @@ from top2vec import Top2Vec
 from gensim.models.coherencemodel import CoherenceModel
 from gensim.corpora import Dictionary
 from octis.evaluation_metrics.diversity_metrics import InvertedRBO
+import pickle
+import gc
 
 
 """
 =============================================================================
 
 To run:
->>> python T2V_HReduce.py --n_start 10 --n_limit 50
+>>> python T2V_HReduce.py --n_start 10 --n_limit 50 --optimization 1
 =============================================================================
 """
 
 parser = argparse.ArgumentParser(description='T2V H_Reduce')
 
 ### data and file related arguments
-parser.add_argument('--model', type=str, default='Models/T2V_model_3', help='Initially trained T2V model')
-parser.add_argument('--save_model', type=str, default='Models/T2V_model_3_H', help='directory to save tranied ETM model')
+parser.add_argument('--documents', type=str, default='Data/T2V_train.csv', help='path to cleaned documents')
+parser.add_argument('--model', type=str, default='Models/T2V_model_4', help='Initially trained T2V model')
+parser.add_argument('--save_model', type=str, default='Models/', help='directory to save tranied ETM model')
 
 ### arguments related to model and training
-parser.add_argument('--n_start', type=int, default=10, help='min number of topics to be trained')
-parser.add_argument('--n_limit', type=int, default=310, help='max number of topics to be trained')
+parser.add_argument('--n_start', type=int, default=20, help='min number of topics to be trained')
+parser.add_argument('--n_limit', type=int, default=300, help='max number of topics to be trained')
 parser.add_argument('--n_step', type=int, default=20, help='step for number of topics')
 parser.add_argument('--topn', type=int, default=15, help='the number of top words to be extracted from each topic')
+
+parser.add_argument('--optimization', type=int, default=1, help='optimization of N (1) or saving the model with optimal N')
 
 args = parser.parse_args()
 
@@ -50,25 +55,53 @@ print(f"{strftime('%D %H:%M', gmtime())} | Loading trained T2V model ... \n")
 
 T2V_model = Top2Vec.load(args.model)
 
-print(f"{strftime('%D %H:%M', gmtime())} | Creating dictionary from traning documents ... \n")
-texts = [doc.split() for doc in T2V_model.documents]
-dictionary = Dictionary(documents=texts)
+if args.optimization ==1:
+    print(f"{strftime('%D %H:%M', gmtime())} | Loading cleaned text data ... \n")
 
-def model_optimizer(model, limit, start, step):
-    """
-    Finding the best number of topics based on the topic quality measures.
-    """
+    def get_docs():
+        docs_df = pd.read_csv(args.documents, parse_dates=['report_dt'])
 
-    # Create initial objects for model metrics and the best model
-    metrics = {}
-    best_model = None
+        # Modifying the sample
+        docs_df = docs_df[(docs_df['filerCIK']==docs_df['CIK'])|(docs_df['ticker'].notna())]
+        SIC_df = pd.read_csv("Data/SIC_df.csv", usecols=['cik', 'sic']).drop_duplicates().dropna().astype(int)
+        SIC_df.rename(columns={'cik': "CIK", 'sic': "SIC"}, inplace=True)
+        docs_df = pd.merge(
+            left=docs_df, 
+            right=SIC_df, 
+            on="CIK", how='left'
+        )
 
-    # Training an ETM instance for the range of topics   
-    for num_topics in range(start, limit, step):
-        model.hierarchical_topic_reduction(num_topics)
-        topics = model.topic_words_reduced
+        fin_sic = [6021, 6022, 6029, 6035, 6036, 6099, 6111, 6141, 6153, 6159, 6162, 6163, 6172, 6189, 
+                6199, 6200, 6211, 6221, 6282, 6311, 6321, 6324, 6331, 6351, 6361, 6399, 6411]
 
-        print(".....")
+        docs_df = docs_df[
+            (docs_df['report_dt']>"2006-01-01")
+            &(docs_df['report_dt']<"2024-01-01")
+            &(docs_df['SIC'].notna())
+            &(~docs_df['SIC'].isin(fin_sic))
+        ] 
+        
+        return docs_df["cleaned_txt"].to_list()
+
+    train_docs = get_docs()
+
+    print(f"{strftime('%D %H:%M', gmtime())} | Creating dictionary from traning documents ... \n")
+    texts = [doc.split() for doc in train_docs]
+    dictionary = Dictionary(documents=texts)
+
+    num_topic_range = range(args.n_start, args.n_limit+1, args.n_step)
+
+    gc.collect()
+
+    def model_optimizer(num_topics):
+        """
+        Finding the best number of topics based on the topic quality measures.
+        """
+        # Create initial objects for model metrics and the best model
+        
+        T2V_model.hierarchical_topic_reduction(num_topics)
+        topics = T2V_model.topic_words_reduced
+
         TC_metric = CoherenceModel(
             topics=topics, 
             texts=texts, 
@@ -83,33 +116,51 @@ def model_optimizer(model, limit, start, step):
         IRBO_metric = InvertedRBO(topk=args.topn)
         IRBO = IRBO_metric.score({"topics": topics})
         
-        metrics[num_topics] = (TC, IRBO)
+        metrics = {'num_topics': num_topics, 'TC': TC, 'IRBO': IRBO}
 
-        print(f"{strftime('%D %H:%M', gmtime())} | Model with {num_topics} topics >>>\
-            \n\t Coherence: {TC:.5f}\
-            \n\t IRBO: {IRBO:.5f}")
+        return metrics
 
+    print(f"{strftime('%D %H:%M', gmtime())} | Topic reduction started ...\n")
+    sys.stdout.flush()
+
+    scores = []
+    for N in num_topic_range:
+        s = model_optimizer(N)
+        print(f"\n{strftime('%D %H:%M', gmtime())} | Model with {N} topics >>>\
+            \n\t Coherence: {s['TC']:.5f}\
+            \n\t IRBO: {s['IRBO']:.5f}")
         sys.stdout.flush()
+        scores.append(s)
+        gc.collect()
 
-    return metrics
 
-print(f"{strftime('%D %H:%M', gmtime())} | Topic reduction started.\n")
-sys.stdout.flush()
+    # Saving quality scores
+    all_scores = pd.DataFrame(scores)
+    all_scores.to_excel(f"T2V_HR_scores_{strftime('%d%m%y', date)}.xlsx")
 
-print(f"{strftime('%D %H:%M', gmtime())} | Results  ...\n")
-scores = model_optimizer(
-    model=T2V_model,
-    limit=args.n_limit+1, 
-    start=args.n_start, 
-    step=args.n_step, 
-)
+else:
+    T2V_model.hierarchical_topic_reduction(110)
 
-# Saving quality scores
-all_scores = pd.DataFrame(scores)
-all_scores.to_excel(f"ETM_opt_scores_{strftime('%d%m%y', date)}.xlsx")
+    T2V_model.save(f"{args.save_model}")
+
+    document_ids = T2V_model.document_ids.tolist()
+    t2v_df = pd.DataFrame(data={"Docs": document_ids, "Topic": T2V_model.doc_top, "Score": T2V_model.doc_dist,
+                                "Topic_H": T2V_model.doc_top_reduced, "Score_H": T2V_model.doc_dist_reduced})
+
+    t2v_df[['CIK', 'Report_dt', 'Filing_dt', 'rf_seq']] = t2v_df["Docs"].str.split(" ", expand=True)
+
+    t2v_df.to_csv("T2V_df_4.csv", index=False)
+
+    topics = T2V_model.topic_words_reduced
+    T2V_tw = pd.DataFrame(data=topics)
+    T2V_tw.to_csv("T2V_tw_4.csv")
+
+    topic_word_dist = T2V_model.topic_word_scores_reduced
+    with open("T2V_wdist_4.pkl", 'wb') as f:
+        pickle.dump(topic_word_dist, f)
+
 
 print(f"{strftime('%D %H:%M', gmtime())} | >>> END <<< \n")
 
 sys.stdout.close()
-
 
--- Top2Vec_train.py
+++ Top2Vec_train.py
@@ -12,22 +12,23 @@ from top2vec import Top2Vec
 from time import strftime, gmtime
 import sys
 import os
+import gc
 
 # =============================================================================
 # This module trains the Top2Vec model on the corpora 
 
 # To run:
-# >>> python Top2Vec_train.py --speed fast-learn
+# >>> python Top2Vec_train.py --speed deep-learn
 # =============================================================================
 
 parser = argparse.ArgumentParser(description='Top2Vec model trainer')
 
 ### data and file related arguments
-parser.add_argument('--documents', type=str, default='Data/clean_docs_3.csv', help='path to cleaned documents')
-parser.add_argument('--save_model', type=str, default='Models/T2V_model_3', help='directory to save tranied model')
+parser.add_argument('--documents', type=str, default='Data/T2V_train.csv', help='path to cleaned documents')
+parser.add_argument('--save_model', type=str, default='Models/T2V_model_4', help='directory to save tranied model')
 
 ### model and optimization related arguments
-parser.add_argument('--speed', type=str, default='deep-learn', help='determines how fast the model trains (fast-learn, learn, deep-learn)')
+parser.add_argument('--speed', type=str, default='learn', help='determines how fast the model trains (fast-learn, learn, deep-learn)')
 parser.add_argument('--njobs', type=int, default=-1, help='number of cpu cores to be used for training')
 parser.add_argument('--batch_size', type=int, default=1000, help='input batch size for training')
 
@@ -42,15 +43,44 @@ print(f"{strftime('%D %H:%M', gmtime())} | <<< START >>> \n")
 
 # Read preprocessed text data as Pandas DataFrame
 print(f"{strftime('%D %H:%M', gmtime())} | Loading cleaned text data ... \n")
-train_docs_df = pd.read_csv(args.documents).reset_index()
+
+def get_docs():
+    docs_df = pd.read_csv(args.documents, parse_dates=['report_dt'])
+
+    # Modifying the sample
+    docs_df = docs_df[(docs_df['filerCIK']==docs_df['CIK'])|(docs_df['ticker'].notna())]
+    SIC_df = pd.read_csv("Data/SIC_df.csv", usecols=['cik', 'sic']).drop_duplicates().dropna().astype(int)
+    SIC_df.rename(columns={'cik': "CIK", 'sic': "SIC"}, inplace=True)
+    docs_df = pd.merge(
+        left=docs_df, 
+        right=SIC_df, 
+        on="CIK", how='left'
+    )
+
+    fin_sic = [6021, 6022, 6029, 6035, 6036, 6099, 6111, 6141, 6153, 6159, 6162, 6163, 6172, 6189, 
+               6199, 6200, 6211, 6221, 6282, 6311, 6321, 6324, 6331, 6351, 6361, 6399, 6411]
+
+    docs_df = docs_df[
+        (docs_df['report_dt']>"2006-01-01")
+        &(docs_df['report_dt']<"2024-01-01")
+        &(docs_df['SIC'].notna())
+        &(~docs_df['SIC'].isin(fin_sic))
+    ] 
+    
+    return docs_df
+
+train_docs_df = get_docs()
+
 train_docs = train_docs_df["cleaned_txt"].to_list()
 
 # Create new index per document
-new_ind = (
-    train_docs_df[["CIK", "report_dt", "filing_dt", "index"]]
-    .applymap(str)
+new_idx = (
+    train_docs_df[["CIK", "report_dt", "filing_dt", "rf_seq"]].astype(str)
     .apply(lambda x: " ".join(x), axis=1)
 )
+
+del train_docs_df
+gc.collect()
 
 
 def tokenizer(text):
@@ -65,24 +95,26 @@ if args.njobs == -1:
 else:
     njobs = args.njobs
 
-print(f"{strftime('%D %H:%M', gmtime())} | Training Top2Vec model started.\n")
+print(f"{strftime('%D %H:%M', gmtime())} | Training Top2Vec model on {len(train_docs)} RFs.\n")
 sys.stdout.flush()
 
 top2vec_model = Top2Vec(
     documents=train_docs, 
-    # embedding_model=model.model,
-    min_count=100,
-    embedding_batch_size = args.batch_size,
+    embedding_model="doc2vec", # "universal-sentence-encoder-large",
+    tokenizer=tokenizer,
+    keep_documents=False, 
+    split_documents=True, 
+    max_num_chunks=4, 
+    topic_merge_delta=0.5,
+    min_count=int(0.0005*len(train_docs)),
+    embedding_batch_size=args.batch_size,
     speed=args.speed, 
     workers=njobs, 
-    document_ids=new_ind.to_list(), 
-    tokenizer=tokenizer,
-    # hdbscan_args = {
-    #     'min_cluster_size': 20,
-    #     'metric': 'euclidean',
-    #     'cluster_selection_method': 'eom',
-    #     'cluster_selection_epsilon' : 0.6
-    # }
+    document_ids=new_idx.to_list(),
+    gpu_hdbscan=True, 
+    gpu_umap=True,
+    umap_args={'unique': True},
+    hdbscan_args = {'min_cluster_size': 20}
 )
 
 print(f"{strftime('%D %H:%M', gmtime())} | Saving trained Top2Vec model to disk ...\n")
